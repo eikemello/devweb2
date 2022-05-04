48c00929de66a822cb4ee6eee63967a9b6858a9c
eike.m <eike.m@sidi.org.br>
2022-05-03 23:24:32 -0300

    feat: format code and ativo/remover

--- museuFinal/app/models/home.js
+++ museuFinal/app/models/home.js
@@ -4,7 +4,7 @@ module.exports = {
         connection.query(querysql, callback);
     },
     setObrasDeArte: function (obra, connection, callback) {
-        sql = `INSERT INTO estoque.ativo (tipo, marca, modelo, disponibilidade, desgaste) VALUES ("${obra.tipo}", "${obra.marca}", "${obra.modelo}", "${obra.disponibilidade}", "${obra.desgaste}");`
+        sql = `INSERT INTO estoque.ativo (tipo, marca, modelo, serial_number, disponibilidade, desgaste) VALUES ("${obra.tipo}", "${obra.marca}", "${obra.modelo}","${obra.serial_number}", "${obra.disponibilidade}", "${obra.desgaste}");`
         connection.query(sql, callback);
     }
 }
--- museuFinal/app/routes/routes.js
+++ museuFinal/app/routes/routes.js
@@ -12,8 +12,8 @@ module.exports = {
     },
     //---------------rotas ativos-------------------------
     registrarAtivo: function (app) {
-        app.get('/ativo/atualizar', function (req, res) {
-            res.render('./ativo/registrar.ejs', { errors: null, obra: { tipo: '', marca: '', modelo: '', desgaste: ''}});
+        app.get('/ativo/registrar', function (req, res) {
+            res.render('./ativo/registrar.ejs', { errors: null});
         });
     },
     salvarAtivo: function (app) {
@@ -25,6 +25,7 @@ module.exports = {
                 tipo: obra.tipo,
                 marca: obra.marca,
                 modelo: obra.modelo,
+                serial_number: obra.serial_number,
                 disponibilidade: "Disponível",//se esta cadastrando o primeiro responsável é sempre o estoque
                 desgaste: obra.desgaste
             }
@@ -39,40 +40,37 @@ module.exports = {
         });
     },
     atualizarAtivo: function (app) {
-        app.get('/ativo/atualizar2', function (req, res) {
-            var q = url.parse(req.url, true);
+        app.get('/ativo/atualizar', function (req, res) {
+            /* var q = url.parse(req.url, true);
             let connection = app.config.dbServer();
             obra = {
                 id_ativo: q.query['id'],
-            }
-            console.log("[controller atualizarAtivo] > ", obra);
-            getObra(obra, connection, function (error, result) {
+            } */
+            console.log("[controller atualizarAtivo] > ");
+            res.render('./ativo/atualizar.ejs', { errors: null});
+            /* getObra(obra, connection, function (error, result) {
                 if(result != 0 & error == null){
                     res.render('obra.ejs', {obra: result});
                 } else {
                     const strResult = JSON.stringify(result);
                     res.render('error.ejs', {error: 'ID de pesquisa '+obra.id+' não encontado! ', result: strResult});                    
                 }
-            });
+            }); */
         });
     },
     removerAtivo: function (app) {
         app.get('/ativo/remover', function (req, res) {
-            var q = url.parse(req.url, true);
-            let connection = app.config.dbServer();
-            obra = {
-                id_ativo: q.query['id'],
-            }
-            console.log("[controller removerAtivo] > ", obra);
-            setDeletarObra(obra, connection, function (error, result) {
+            console.log("[controller removerAtivo] > ");
+            res.render('./ativo/remover.ejs');
+            /* setDeletarObra(obra, connection, function (error, result) {
                 if(result.affectedRows > 0){
-                    //res.render('obra.ejs', {obra: obra});
+                    res.render('obra.ejs', {obra: obra});
                     homeListar(app, req, res);
                 } else {
                     const strResult = JSON.stringify(result);
                     res.render('error.ejs', {error: 'Nao deletado! Provável ID inexistente. ', reuslt: strResult});                    
                 }
-            });
+            }); */
         });
     },
     //-----------------rotas transferência---------------------
