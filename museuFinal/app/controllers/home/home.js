304bfde0bfaa6dcba3cd20beaab58a2f8fc93327
eike.m <eike.m@sidi.org.br>
2022-05-21 20:12:32 -0300

    feat: dashboard improve with table

--- museuFinal/app/controllers/home/home.js
+++ museuFinal/app/controllers/home/home.js
@@ -1,16 +1,16 @@
-const { getAtivo: getObrasDeArte } = require('../../models/home');
+const { getAtivo } = require('../../models/home');
 const logger = require('../../../config/logger');
 
 module.exports.homeListar = function (app, req, res) {
     let connection = app.config.dbServer();
-    getObrasDeArte(connection, function (error, result) {
+    getAtivo(connection, function (error, result) {
         if (error) {
             logger.log({
                 level: 'error',
                 message: error.message,
                 timestamp: Date.now()
             });
-            res.send("Problemas com a conexão!!! " + error.message);
+            res.send("Problemas com a consulta aos dados!!! " + error.message);
         } else {
             res.render('home.ejs', { ativo: result });
         }
