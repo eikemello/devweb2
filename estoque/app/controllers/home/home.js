a9b7a1a02ef08c7e1594bcfa574c16d8702f26ee
eike.m <eike.m@sidi.org.br>
2022-06-09 16:27:17 -0300

    feat: transfer table on home page

--- estoque/app/controllers/home/home.js
+++ estoque/app/controllers/home/home.js
@@ -1,4 +1,4 @@
-const { getAtivo, getEmprestimo } = require('../../models/home');
+const { getAtivo, getEmprestimo, getTransferencia } = require('../../models/home');
 const logger = require('../../../config/logger');
 
 module.exports.homeListar = function (app, req, res) {
@@ -10,7 +10,7 @@ module.exports.homeListar = function (app, req, res) {
                 message: error.message,
                 timestamp: Date.now()
             });
-            res.send("Problemas com a consulta aos dados!!! " + error.message);
+            res.send("Problemas com a conexão: " +error.message);
         } else {
             const result_ativo = result;
             getEmprestimo(connection, function (error, result) {
@@ -23,8 +23,18 @@ module.exports.homeListar = function (app, req, res) {
                     res.send("Problemas com a conexão: " + error.message);
                 } else {
                     const result_emprestimo = result;
-                    console.log(result_emprestimo);
-                    res.render('home.ejs', { ativo: result_ativo, emprestimo: result_emprestimo });
+                    getTransferencia(connection, function (error, result) {
+                        if (error) {
+                            logger.log({
+                                level: 'error',
+                                message: error.message,
+                                timestamp: Date.now()
+                            });
+                            res.send("Problemas com a conexão: " + error.message);
+                        } else {
+                            res.render('home.ejs', { ativo: result_ativo, emprestimo: result_emprestimo, transferencia: result});
+                        }
+                    });
                 }
             });
         }
--- estoque/app/models/home.js
+++ estoque/app/models/home.js
@@ -1,14 +1,19 @@
+var querysql = null;
 module.exports = {
     getAtivo: function (connection, callback) {
-        let querysql = 'select * from ativo;';
+        querysql = 'select * from ativo;';
         connection.query(querysql, callback);
     },
     getEmprestimo: function (connection, callback) {
-        let emprestado = 'SELECT P.tipo, P.modelo, P.serial_number, C.nome FROM ativo P INNER JOIN funcionario C ON P.id_ativo = C.id_funcionario;';
-        connection.query(emprestado, callback);
+        querysql = 'SELECT A.tipo, A.modelo, A.serial_number, F.nome FROM ativo A INNER JOIN funcionario F ON A.id_ativo = F.id_funcionario;';
+        connection.query(querysql, callback);
+    },
+    getTransferencia: function (connection, callback) {
+        querysql = 'SELECT T.id_transfererencia, T.id_novo_responsavel,  T.id_antigo_responsavel, F.nome, A.tipo, A.serial_number FROM ativo A INNER JOIN transferencia T ON T.id_ativo = A.id_ativo JOIN funcionario F ON T.id_novo_responsavel = F.id_funcionario;';
+        connection.query(querysql, callback);
     },
     setAtivo: function (obra, connection, callback) {
-        let sql = `INSERT INTO estoque.ativo (tipo, marca, modelo, serial_number, disponibilidade, desgaste) VALUES ("${obra.tipo}", "${obra.marca}", "${obra.modelo}","${obra.serial_number}", "${obra.disponibilidade}", "${obra.desgaste}");`
-        connection.query(sql, callback);
+        querysql = `INSERT INTO estoque.ativo (tipo, marca, modelo, serial_number, disponibilidade, desgaste) VALUES ("${obra.tipo}", "${obra.marca}", "${obra.modelo}","${obra.serial_number}", "${obra.disponibilidade}", "${obra.desgaste}");`
+        connection.query(querysql, callback);
     }
 }
